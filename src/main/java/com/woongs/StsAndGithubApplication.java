2936c26ad74b415388cf861f0e8c65ddc9a9dbe6
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-11-23 10:39:20 +0900

    STS와 GITHUB이 연동이 되는건가요?

---
+++ src/main/java/com/woongs/StsAndGithubApplication.java
@@ -0,0 +1,12 @@
+package com.woongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsAndGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsAndGithubApplication.class, args);
+	}
+}
